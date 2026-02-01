c3df41a07cae96be02e2bebf91f883cdeda0c340
SebastiianSb <cbastian.sb@gmail.com>
2026-01-31 21:50:08 -0500

    sumInvoices function was created to calculated the total

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const unitPrice = document.getElementById('price-input')
 
 const totalCalculated = document.getElementById('price-calculated-label')
 
+const stackBody = document.getElementById('stack-table-body')
+const queueBody = document.getElementById('queue-table-boby')
+const listBody = document.getElementById('list-table-body')
+
+const btnRegisterReceipt = document.getElementById('btn-register')
+
 
 unitPrice.addEventListener('input', function(){
     totalCalculated.textContent = unitPrice.value
@@ -12,4 +18,30 @@ unitPrice.addEventListener('input', function(){
         totalCalculated.textContent = 0.00
     }
 })
+
+
+btnRegisterReceipt.addEventListener('click', function(){
+    sumInvoices(unitPrice.value,quantityInput.value)
+})
+
+function sumInvoices(uPrice,quantityIn){
+
+    let totalCalculated = (uPrice * quantityIn)
+    stackBody.textContent = totalCalculated
+}
+
+class Stack{
+    constructor(){
+        this.newStack = []
+    }
+    stacking(e){
+        this.newStack.push(e)
+    }
+    unstack(){
+        this.newStack.pop
+    }
+}
+const news = new Stack()
+
+
 
