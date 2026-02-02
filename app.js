9128b50a312af41d35315527a2c2c162e013783b
SebastiianSb <cbastian.sb@gmail.com>
2026-02-01 20:06:46 -0500

    Strack Data strucures logic was added

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const usernameInput = document.getElementById('username-input')
 const conceptInput = document.getElementById('concept')
 const quantityInput = document.getElementById('quantity-input')
-const unitPrice = document.getElementById('price-input')
+const unitPriceInput = document.getElementById('price-input')
 
 const totalCalculated = document.getElementById('price-calculated-label')
 
@@ -12,36 +12,117 @@ const listBody = document.getElementById('list-table-body')
 const btnRegisterReceipt = document.getElementById('btn-register')
 
 
-unitPrice.addEventListener('input', function(){
-    totalCalculated.textContent = unitPrice.value
-    if(unitPrice.value === ''){
+unitPriceInput.addEventListener('input', function(){
+    totalCalculated.textContent = unitPriceInput.value
+    if(unitPriceInput.value === ''){
         totalCalculated.textContent = 0.00
     }
 })
 
+let idCounter = 0
 
 btnRegisterReceipt.addEventListener('click', function(){
-    sumInvoices(unitPrice.value,quantityInput.value)
+
+    const selectorStack = document.querySelector('input[id="struct-stack"]:checked')
+    const selectorQueue = document.querySelector('input[id="struct-queue]:checked')
+    const selectorList = document.querySelector('input[id="struct-list]:checked')
+
+    idCounter++
+    if(selectorStack){
+        news.stack(idCounter,usernameInput.value,conceptInput.value,quantityInput.value,unitPriceInput.value,sumInvoices(unitPriceInput.value,quantityInput.value))
+        news.printStack()
+    }
+    if(selectorQueue){
+        newq.enqueue(idCounter,usernameInput.value,conceptInput.value,quantityInput.value,unitPriceInput.value,sumInvoices(unitPriceInput.value,quantityInput.value))
+        newq.printQueue()
+    }
+    if(selectorList){
+
+    }
 })
 
 function sumInvoices(uPrice,quantityIn){
 
     let totalCalculated = (uPrice * quantityIn)
-    stackBody.textContent = totalCalculated
+    return totalCalculated
 }
 
 class Stack{
     constructor(){
         this.newStack = []
     }
-    stacking(e){
-        this.newStack.push(e)
+    stack(e1,e2,e3,e4,e5,e6){  //(e) means element
+
+        const objStack = {
+            id: e1,
+            client: e2,
+            concept: e3,
+            quantity: e4,
+            price: e5,
+            total: e6
+        }
+
+        this.newStack.push(objStack)
     }
-    unstack(){
-        this.newStack.pop
+    unstack(objStack){
+        this.newStack.pop(objStack)
+    }
+    printStack(){
+        stackBody.textContent = ''
+
+        this.newStack.forEach((objStack) => {
+            const stackPrint = `
+            <tr>
+                <td>${objStack.id}</td>
+                <td>${objStack.client}</td>
+                <td>${objStack.concept}</td>
+                <td>${objStack.quantity}</td>
+                <td>${objStack.price}</td>
+                <td>${objStack.total}</td>
+            </tr>
+            `
+        stackBody.insertAdjacentHTML('afterbegin',stackPrint)
+
+        })
     }
 }
 const news = new Stack()
 
+class Queue {
+    constructor(){
+        this.newQueue = []
+    }
+    enqueue(e1,e2,e3,e4,e5,e6){
+        const objQueue = {
+            id: e1,
+            client: e2,
+            concept: e3,
+            quantity: e4,
+            price: e5,
+            total: e6
+        }
+        this.newQueue.push(objQueue)
+    }
+    dequeue(){
+        this.newQueue.shift(objQueue)
+    }
+    printQueue(){
+        queueBody.textContent = ''
 
+        this.newQueue.forEach((objQueue) => {
+            const queuePrint = `
+            <tr>
+                <td>${objQueue.id}</td>
+                <td>${objQueue.client}</td>
+                <td>${objQueue.concept}</td>
+                <td>${objQueue.quantity}</td>
+                <td>${objQueue.price}</td>
+                <td>${objQueue.total}</td>
+            </tr>
+            `
+        queueBody.insertAdjacentElement('beforeend', queuePrint)
+        })
+    }
+}
+const newq = new Queue()
 
